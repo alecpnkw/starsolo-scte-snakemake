2e1894ca9d6b3640722b6bf33da4d4ae81e99c8b
Alec Pankow <alec.pankow@gmail.com>
2025-09-10 15:03:08 -0400

    draft using sample sheet

--- workflow/scripts/gather_fastq.py
+++ workflow/scripts/gather_fastq.py
@@ -5,11 +5,11 @@ from typing import List, Tuple, Dict
 
 # --- Configuration ---
 
+
 COMP_EXTS = (".gz", ".bz2", ".zst", ".xz")
 BASE_EXTS = (".fastq", ".fq")
 
 # Try explicit tokens first; then bare _1/_2 (no lookbehind needed).
-
 READ_TOKEN_PATTERNS = [
     re.compile(r"([._-])R(?P<read>[12])([._-]|$)", re.IGNORECASE),     # _R1_
     re.compile(r"([._-])read(?P<read>[12])([._-]|$)", re.IGNORECASE),  # _read1_
@@ -100,80 +100,26 @@ def find_fastq_pairs(input_dir: str, recursive: bool = True
 
     return r1_paths, r2_paths, {"R1": unmatched_R1, "R2": unmatched_R2}
 
-rule star_genome_generate:
-    input:
-        fasta = config["genome"]["fasta"],
-        gtf = config["genome"]["gtf"]
-    output:
-        directory("resources/star_genome_generate/{genome}")
-    threads: 4
-    conda: "../envs/star-scte.yaml"
-    resources:
-        mem_mb = 40000,
-        disk_mb = 50000,
-        walltime = 360
-    shell:
-        """
-        STAR --runMode genomeGenerate \
-            --runThreadN {threads} \
-            --genomeDir {output} \
-            --genomeFastaFiles {input.fasta} \
-            --sjdbGTFfile {input.gtf} \
-            --genomeSAsparseD 3
-        """
 
-def STARSOLO_INPUT(wildcards):
-    fastq_dir = samples.loc[wildcards.dataset, "fastqs"]
-    r1, r2, unmatched = find_fastq_pairs(fastq_dir)
-    return {
-        "R1_fastqs": r1,
-        "R2_fastqs": r2,
-        "genome": "resources/star_genome_generate/{genome}"
-    }
+# --- Example ---
+# if __name__ == "__main__":
+#     r1, r2, unmatched = find_fastq_pairs("/path/to/fastqs")
+#     print("Pairs:")
+#     for a, b in zip(r1, r2):
+#         print(a, "<->", b)
+#     print("Unmatched:", unmatched)
 
-rule starsolo:
-    input: unpack(STARSOLO_INPUT)
-    output:
-        # see STAR manual for additional output files
-        "results/starsolo/{dataset}_{genome}/Aligned.sortedByCoord.out.bam",
-        "results/starsolo/{dataset}_{genome}/SJ.out.tab"
-    params:
-        whitelist = config["umi_whitelist"],
-        prefix = "results/starsolo/{dataset}_{genome}/",
-        fastq_str = lambda wc, input: ",".join(input["R2_fastqs"]) + " " + ",".join(input["R1_fastqs"]),
-        CBstart = config["soloCBstart"],
-        CBlen = config["soloCBlen"],
-        UMIstart = config["soloUMIstart"],
-        UMIlen = config["soloUMIlen"],
-        BCReadlen = config["soloBarcodeReadLength"]
-    conda: "../envs/star-scte.yaml"
-    threads: 12
-    resources:
-        mem_mb = 48000,
-        disk_mb = 164000,
-        walltime = 720
-    shell:
-        """
-        STAR --runThreadN 48 \
-            --soloType CB_UMI_Simple \
-            --soloCBwhitelist {params.whitelist} \
-            --soloCBstart {params.CBstart} \
-            --soloCBlen {params.CBlen} \
-            --soloUMIstart {params.UMIstart} \
-            --soloUMIlen {params.UMIlen} \
-            --soloBarcodeReadLength {params.BCReadlen} \
-            --genomeDir {input.genome} \
-            --readFilesIn {params.fastq_str} \
-            --readFilesCommand zcat \
-            --outSAMattributes NH HI nM AS CR CY UR UY CB UB GX GN sS sQ sM \
-            --outSAMtype BAM SortedByCoordinate \
-            --soloUMIfiltering MultiGeneUMI \
-            --soloCBmatchWLtype 1MM_multi_pseudocounts \
-            --limitBAMsortRAM 16111457846 \
-            --outFilterMultimapNmax 100 \
-            --winAnchorMultimapNmax 100 \
-            --outSAMmultNmax 1 \
-            --twopassMode Basic \
-            --runRNGseed 42 \
-            --outFileNamePrefix {params.prefix}
-        """
+
+if __name__ == "__main__":
+    r1, r2, unmatched = find_fastq_pairs(sys.argv[1], recursive=True)
+
+    print("Pairs:")
+    for a, b in zip(r1, r2):
+        print(a, "<->", b)
+    print("Unmatched:", unmatched)
+
+    with open(sys.argv[2], 'w') as f:
+        f.write('\n'.join(r1))
+
+    with open(sys.argv[3], 'w') as f:
+        f.write('\n'.join(r2))
